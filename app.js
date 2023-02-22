9e355397605cbf0ffdc639a840335ad1975057ab
John Murianki <mcjthiongo@gmail.com>
2023-02-22 18:30:15 +0300

    fix linter errors

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
+/* eslint-disable no-bitwise */
 class BookStore {
-    constructor() {
-        this.books = [];
-    }
+  constructor() {
+    this.books = [];
+  }
 
-    addBook(title, author) {
-        const book = {
-            title: title,
-            author: author
-        };
-        this.books.push(book);
-        return book;
-    }
+  addBook(title, author) {
+    const book = {
+      title,
+      author,
+    };
+    this.books.push(book);
+    return book;
+  }
 
-    removeBook(index) {
-        this.books.splice(index, 1);
-    }
+  removeBook(index) {
+    this.books.splice(index, 1);
+  }
 
-    get allBooks() {
-        return this.books;
-    }
+  get allBooks() {
+    return this.books;
+  }
 
-    set allBooks(books) {
-        this.books = books;
-    }
+  set allBooks(books) {
+    this.books = books;
+  }
 }
 
 const addButton = document.getElementById('addButton');
@@ -33,51 +34,49 @@ const bookItems = document.getElementById('display');
 const bookshelf = new BookStore();
 
 function displayBooks(books) {
-    bookItems.innerHTML = '';
+  bookItems.innerHTML = '';
 
-    books.forEach((book, index) => {
-        const itemDiv = document.createElement('ul')
-        itemDiv.className = "displayFlex displayItemStyle"
-        if (index % 2 === 0) { itemDiv.className = "displayFlex displayItemStyle backgroundColor" }
-        const bookItem = document.createElement('li');
-        bookItem.className = "itemAdd"
-        const remLi = document.createElement('li');
-        remLi.className = "itemAdd"
-        const deleteBtn = document.createElement('button');
-        deleteBtn.className = "btn  btnStyle"
-        deleteBtn.innerHTML = "Remove"
-        bookItem.textContent = `"${book.title}" by ${book.author}`;
+  books.forEach((book, index) => {
+    const itemDiv = document.createElement('ul');
+    itemDiv.className = 'displayFlex displayItemStyle';
+    if (index % 2 === 0) { itemDiv.className = 'displayFlex displayItemStyle backgroundColor'; }
+    const bookItem = document.createElement('li');
+    bookItem.className = 'itemAdd';
+    const remLi = document.createElement('li');
+    remLi.className = 'itemAdd';
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn  btnStyle';
+    deleteBtn.innerHTML = 'Remove';
+    bookItem.textContent = `"${book.title}" by ${book.author}`;
 
-        bookItems.appendChild(itemDiv);
-        itemDiv.appendChild(bookItem);
-        itemDiv.appendChild(remLi);
-        remLi.appendChild(deleteBtn);
+    bookItems.appendChild(itemDiv);
+    itemDiv.appendChild(bookItem);
+    itemDiv.appendChild(remLi);
+    remLi.appendChild(deleteBtn);
 
-        deleteBtn.addEventListener('click', () => {
-            bookshelf.removeBook(index);
-            localStorage.setItem('books', JSON.stringify(bookshelf.allBooks));
-            displayBooks(bookshelf.allBooks);
-        });
+    deleteBtn.addEventListener('click', () => {
+      bookshelf.removeBook(index);
+      localStorage.setItem('books', JSON.stringify(bookshelf.allBooks));
+      displayBooks(bookshelf.allBooks);
     });
+  });
 }
 
 const storedBooks = JSON.parse(localStorage.getItem('books'));
 if (storedBooks) {
-    bookshelf.allBooks = storedBooks;
+  bookshelf.allBooks = storedBooks;
 }
 
 displayBooks(bookshelf.allBooks);
 
 addButton.addEventListener('click', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (bookTitle.value !== '' & bookAuthor.value !== "") {
-        bookshelf.addBook(bookTitle.value, bookAuthor.value);
-        localStorage.setItem('books', JSON.stringify(bookshelf.allBooks));
-        displayBooks(bookshelf.allBooks);
-        bookTitle.value = '';
-        bookAuthor.value = '';
-    }
-
-
+  if (bookTitle.value !== '' & bookAuthor.value !== '') {
+    bookshelf.addBook(bookTitle.value, bookAuthor.value);
+    localStorage.setItem('books', JSON.stringify(bookshelf.allBooks));
+    displayBooks(bookshelf.allBooks);
+    bookTitle.value = '';
+    bookAuthor.value = '';
+  }
 });
